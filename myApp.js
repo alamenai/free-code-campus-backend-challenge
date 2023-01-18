d4018b0d8a3198f89e1959260333680177fb2df0
Ala Eddine <ala.official.acc@gmail.com>
2023-01-18 12:05:30 +0100

    :heavy_check_mark: Serve static assets

--- myApp.js
+++ myApp.js
@@ -5,4 +5,8 @@ let app = express();
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+// Serve static assets
+app.use('/', express.static(__dirname + '/public'));
+
 module.exports = app;
