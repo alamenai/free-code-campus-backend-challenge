ad73c93710fefce2335edfde56d3158448d85034
Ala Eddine <ala.official.acc@gmail.com>
2023-01-18 13:45:20 +0100

    :heavy_check_mark: Implement a logger middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ let app = express();
 // To load environment variables
 require('dotenv').config();
 
+app.use(function (req, res, next) {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});
+
 // Serve Hello World to match the root /
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -17,7 +22,7 @@ app.get('/json', function (req, res) {
   const data = {
     message: 'Hello json',
   };
-  if ( process.env.MESSAGE_STYLE === 'uppercase') {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
     data.message = data.message.toUpperCase();
   }
 
