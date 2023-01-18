1bce2cce80253c522477104a1934b7a3d396c3eb
Ala Eddine <ala.official.acc@gmail.com>
2023-01-18 14:51:21 +0100

    :outbox_tray: Mount the bodyparser module

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
 // To load environment variables
 require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
   console.log(req.method + ' ' + req.path + ' - ' + req.ip);
