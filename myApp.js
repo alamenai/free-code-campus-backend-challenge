550bd93f16d32b696b7f904297364a234dc179cf
Ala Eddine <ala.official.acc@gmail.com>
2023-01-18 11:44:43 +0100

    :Wrecnh: Fix typo

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 
 // Serve Hello World to match the root /
 app.get('/', function (req, res) {
-  console.log('Hello World');
+  console.log('Hello Express');
   res.end();
 });
 module.exports = app;
